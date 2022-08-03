a72f6f29afce2e63b5c1af2c7e033fc9755e6f8d
raul <develop.raul@gmail.com>
2022-08-03 18:57:12 +0900

    [ADD] AssignIpQueryController 구현 및 이에 필요한 객체 작성
    - AssignIpQueryService 작성
    - DataQueryProperty 작성
    - AssignIpQueryResponse 작성
    - PagableAssignIpQueryResponse 작성

---
+++ src/main/kotlin/site/iplease/aimserver/domain/query/service/AssignIpQueryService.kt
@@ -0,0 +1,12 @@
+package site.iplease.aimserver.domain.query.service
+
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import reactor.core.publisher.Mono
+import site.iplease.aimserver.global.common.data.dto.AssignIpDto
+
+interface AssignIpQueryService {
+    fun getAllAssignIp(page: PageRequest): Mono<Page<AssignIpDto>>
+    fun getAllAssignIpByAssigneeId(page: PageRequest, assigneeId: Long): Mono<Page<AssignIpDto>>
+    fun getAssignIpById(assignIpId: Long): Mono<AssignIpDto>
+}
