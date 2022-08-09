87005b5e0e6da205a9333a714422ec5053e1950f
raul <develop.raul@gmail.com>
2022-08-09 18:54:40 +0900

    [ADD] 기존 로직에 error 컴포넌트 적용

--- src/main/kotlin/site/iplease/aimserver/global/common/exception/AlreadyAssignedIpAddressException.kt
+++ src/main/kotlin/site/iplease/aimserver/global/common/exception/AlreadyAssignedIpAddressException.kt
@@ -1,3 +1,10 @@
 package site.iplease.aimserver.global.common.exception
 
-class AlreadyAssignedIpAddressException(message: String): RuntimeException("이미 할당된 IP주소입니다! - $message")
+import site.iplease.aimserver.global.error.IpleaseError
+
+class AlreadyAssignedIpAddressException(private val errorDetail: String): RuntimeException("$ERROR_MESSAGE - $errorDetail"), IpleaseError {
+    companion object { private const val ERROR_MESSAGE = "이미 할당된 IP주소입니다!" }
+
+    override fun getErrorMessage() = ERROR_MESSAGE
+    override fun getErrorDetail() = errorDetail
+}
--- src/main/kotlin/site/iplease/aimserver/global/common/exception/WrongSubnetAddressException.kt
+++ src/main/kotlin/site/iplease/aimserver/global/common/exception/WrongSubnetAddressException.kt
@@ -1,3 +1,10 @@
 package site.iplease.aimserver.global.common.exception
 
-class WrongSubnetAddressException(message: String): RuntimeException("잘못된 서브넷 주소입니다! = $message")
+import site.iplease.aimserver.global.error.IpleaseError
+
+class WrongSubnetAddressException(private val errorDetail: String): RuntimeException("$ERROR_MESSAGE - $errorDetail"), IpleaseError {
+    companion object { private const val ERROR_MESSAGE = "잘못된 서브넷 주소입니다!" }
+
+    override fun getErrorMessage() = ERROR_MESSAGE
+    override fun getErrorDetail() = errorDetail
+}
